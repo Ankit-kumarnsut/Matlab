a826b8630662cb2657d42367fcc3092172eb9539
Ankit-kumarnsut <121795242+Ankit-kumarnsut@users.noreply.github.com>
2023-08-28 14:50:22 +0530

    Add files via upload

---
+++ Ankit kumar 2022UEE4190  TARIQ Aziz 2022UEE4670.m
@@ -0,0 +1,194 @@
+% a=3;
+% b=5;
+% c=a+b
+
+% a=3;
+% b=a;
+% b
+
+% a=3;
+% b=9;
+% c=2*a+b^2-a*b+b/a-10
+
+% a=3;
+% a=a+1;
+% a
+
+% fprintf('hello')
+
+% a=3;
+% b=a*a;
+% c=a*a*a;
+% d=sqrt(a);
+% fprintf('%4u square equals %4u \r',a,b)
+% fprintf('%4u cube equals %4u \r',a,c)
+% fprintf('the square root of %2u is %6.4f \r',a,d)
+
+% a=[3 6 7];
+% b=[1 9 4];
+% c=a+b
+
+% A=3;
+% B=A*A;
+% C=A+B
+
+% summation1=1+3+5+7+9+11
+
+% x=pi;
+% y=sin(pi/2)
+% z=exp(-sin(pi/2))
+
+% x=[0:0.1:20];
+% y=sin(x);
+% plot(x,y
+
+% b=3;
+% for k=1:5
+%     b
+% end
+
+% b=3;
+% for k=1:5
+%     b^k
+% end
+
+% sum1=0;
+% for k=1:9
+%     sum1=sum1+k;
+% end
+% sum1
+
+% sum1=0;
+% for k=1:2:9
+%     sum1=sum1+k;
+% end
+% sum1
+
+
+
+% sum1sum1b=[3 8 9 4 7 5];
+% sum1=0;
+% for k=1:4
+%     sum1=sum1+b(k);
+% end
+% sum1
+
+% b=[3 8 9 4 7 5];
+% sum1=0;
+% for k=1:2:5
+%     sum1=sum1+b(k);
+% end
+% sum1
+
+% sum1=0;
+% for n=1:2
+%     for m=1:3
+%         sum1=sum1+n*m;
+%     end
+% end
+% sum1
+
+% for n=1:2
+%     for m=1:3
+%         fprintf('n=%3u m=%3U \r',n,m)
+%     end
+% end
+
+% b=[2 5 7 4 9 8 3];
+% c=[2 3 5 7];
+% sum1=0;
+% for k=1:4
+%     sum1=sum1+b(c(k));
+% end
+% sum1
+
+% num1=7;
+% if(num1>5)
+%     fprintf('%4u is greater than 5 \r',num1)
+% else
+%     fprintf('%4u is less than or equal to 5\r',num1)
+% end
+
+
+
+
+
+
+
+
+% num1=4;
+% if(num1>=5)
+%     fprintf('%4i is greater than or equal to 5\r',num1)
+% elseif(num1>1)
+%     fprintf('%4i is less than 5 but greater than 1\r',num1)
+% elseif(num1 ==1)
+%     fprintf('%4i is equal 1\r',num1)
+% elseif(num1>-3)
+%     fprintf('%4i is less than 1 but greater than -3\r',num1)
+% else
+%     fprintf('%4i is less than or equal to -3\r',num1)
+% end
+ 
+%     nyear = 1975;
+% if (mod(nyear, 400) == 0)
+%  fprintf('%6u is a leap year', nyear)
+% elseif (mod(nyear,4) == 0) & (mod(nyear,100) ~= 0) 
+%  fprintf('%6u is a leap year', nyear)
+% else
+%  fprintf('%6u is not a leap year', nyear)
+% end
+
+
+% 
+
+% x = 3;
+% while (x < 100)
+%  x = x*3;
+% end
+% x
+
+% a = [2 12 25];
+% b = [3 7 4];
+% c = a+b
+% 
+
+% a = [3 4; 1 6];
+% b = [5 2; 11 7];
+% c = a+b
+
+% a = [3 5; 1 4];
+% b = 2*a
+
+% a = [2 3 5];
+% b = [2 4 9];
+% c = a.*b
+
+% a = [2 3; 1 4];
+% b = [5 1; 7 2];
+% c = a.*b
+
+
+% a = [2 3; 1 4];
+% b = [5 1; 7 2];
+% c = a*b
+
+
+% a = [2 3 5];
+% b = sin(a)
+
+% a = [2 3 5];
+% b = 2*a.^2+3*a+4
+
+% A = [3 5; 2 4];
+% c = A(2,2)+A(1,2)
+
+
+% A = [3 5; 2 4];
+% norm1 = 0;
+% for m = 1:2
+% for n = 1:2
+%  norm1 = norm1+A(m,n)^2;
+% end
+% end
+% norm1 = sqrt(norm1)
+
